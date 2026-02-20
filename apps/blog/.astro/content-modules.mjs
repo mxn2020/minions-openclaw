2d53664eb608909f5d917c03def3d89ede73f2f2
Mehdi Nabhani <mehdi@nabhani.de>
2026-02-20 16:48:23 +0100

    udpates

---
+++ apps/blog/.astro/content-modules.mjs
@@ -0,0 +1,6 @@
+
+export default new Map([
+["src/content/posts/building-with-minions-sdk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fbuilding-with-minions-sdk.mdx&astroContentModuleFlag=true")],
+["src/content/posts/introducing-minions.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fintroducing-minions.mdx&astroContentModuleFlag=true")],
+["src/content/posts/python-sdk-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fpython-sdk-release.mdx&astroContentModuleFlag=true")]]);
+		
